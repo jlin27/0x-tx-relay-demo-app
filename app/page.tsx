3134417af1dbbc5c8f265e87f02b12661481d6bc
Jessica Lin <jlin2700@gmail.com>
2023-11-06 17:19:12 -0800

    Add customize connect button, begin setup to check approvals

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,20 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useEffect, useState } from "react";
 import { formatUnits, parseUnits } from "ethers";
-import useSWR from "swr";
+import { useAccount, useBalance, type Address } from "wagmi";
 import qs from "qs";
 
-function Page() {
+export default function Page() {
   // const priceData = await getTxRelayPrice();
   // console.log(priceData);
+
+  const { address } = useAccount();
+  const takerAddress = address;
 
   const [sellAmount, setSellAmount] = useState("");
   const [buyAmount, setBuyAmount] = useState("");
   const [tradeDirection, setTradeDirection] = useState("sell");
+  const [finalize, setFinalize] = useState(false);
 
   console.log(sellAmount, 6, "<-sellAmount, decimals");
   const parsedSellAmount =
@@ -25,27 +29,39 @@ function Page() {
       ? parseUnits(buyAmount, 18).toString()
       : undefined;
 
-  // Fetch price data
+  // Fetch price data and set the buyAmount whenever the sellAmount changes
   useEffect(() => {
     const params = {
       sellToken: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", // USDC
       buyToken: "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270", // WMATIC
       sellAmount: parsedSellAmount,
       buyAmount: parsedBuyAmount,
-      takerAddress: "0x4D2A422dB44144996E855ce15FB581a477dbB947",
+      takerAddress,
     };
 
     async function main() {
       const response = await fetch(`/api/price?${qs.stringify(params)}`);
       const data = await response.json();
-
       console.log(data, typeof data);
-      // console.log(data.buyAmount, typeof data.buyAmount);
-      // console.log(formatUnits(data.buyAmount, 18), data);
+      console.log(data.buyAmount, typeof data.buyAmount);
+      console.log(formatUnits(data.buyAmount, 18), data);
+      setBuyAmount(formatUnits(data.buyAmount, 18));
     }
 
-    main();
-  }, [sellAmount, parsedBuyAmount, parsedSellAmount]);
+    if (sellAmount !== "") {
+      main();
+    }
+  }, [sellAmount, parsedBuyAmount, parsedSellAmount, takerAddress]);
+
+  const { data, isError, isLoading } = useBalance({
+    address: takerAddress,
+    token: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", // USDC
+  });
+
+  const disabled =
+    data && sellAmount ? parseUnits(sellAmount, 6) > data.value : true;
+
+  console.log(sellAmount);
 
   return (
     <div>
@@ -84,14 +100,131 @@ function Page() {
         />
         <label htmlFor="sell">WMATIC</label>
         <br />
-        <button>Review order</button>
+
+        {/* Add custom button */}
+        <div>{takerAddress}</div>
+
+        {takerAddress ? (
+          <ApproveOrReviewButton
+            sellTokenAddress="0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
+            takerAddress={takerAddress}
+            onClick={() => {
+              setFinalize(true);
+            }}
+            disabled={disabled}
+          />
+        ) : (
+          <ConnectButton.Custom>
+            {({
+              account,
+              chain,
+              openAccountModal,
+              openChainModal,
+              openConnectModal,
+              authenticationStatus,
+              mounted,
+            }) => {
+              // Note: If your app doesn't use authentication, you
+              // can remove all 'authenticationStatus' checks
+              const ready = mounted && authenticationStatus !== "loading";
+              const connected =
+                ready &&
+                account &&
+                chain &&
+                (!authenticationStatus ||
+                  authenticationStatus === "authenticated");
+
+              return (
+                <div
+                  {...(!ready && {
+                    "aria-hidden": true,
+                    style: {
+                      opacity: 0,
+                      pointerEvents: "none",
+                      userSelect: "none",
+                    },
+                  })}
+                >
+                  {(() => {
+                    if (!connected) {
+                      return (
+                        <button onClick={openConnectModal} type="button">
+                          Connect Wallet
+                        </button>
+                      );
+                    }
+
+                    if (chain.unsupported) {
+                      return (
+                        <button onClick={openChainModal} type="button">
+                          Wrong network
+                        </button>
+                      );
+                    }
+
+                    return (
+                      <div style={{ display: "flex", gap: 12 }}>
+                        <button
+                          onClick={openChainModal}
+                          style={{ display: "flex", alignItems: "center" }}
+                          type="button"
+                        >
+                          {chain.hasIcon && (
+                            <div
+                              style={{
+                                background: chain.iconBackground,
+                                width: 12,
+                                height: 12,
+                                borderRadius: 999,
+                                overflow: "hidden",
+                                marginRight: 4,
+                              }}
+                            >
+                              {chain.iconUrl && (
+                                <img
+                                  alt={chain.name ?? "Chain icon"}
+                                  src={chain.iconUrl}
+                                  style={{ width: 12, height: 12 }}
+                                />
+                              )}
+                            </div>
+                          )}
+                          {chain.name}
+                        </button>
+
+                        <button onClick={openAccountModal} type="button">
+                          {account.displayName}
+                          {account.displayBalance
+                            ? ` (${account.displayBalance})`
+                            : ""}
+                        </button>
+                      </div>
+                    );
+                  })()}
+                </div>
+              );
+            }}
+          </ConnectButton.Custom>
+        )}
       </form>
     </div>
   );
 }
 
-export default Page;
-
+function ApproveOrReviewButton({
+  takerAddress,
+  onClick,
+  sellTokenAddress,
+  disabled,
+}: {
+  takerAddress: Address;
+  onClick: () => void;
+  sellTokenAddress: Address;
+  disabled?: boolean;
+}) {
+  // TODO: Look up if the sellToken supports permit. If not (WMATIC), set allowance and pay for approvals with gas. If it does (USDC), just get the Tx Relay price
+  return <></>;
+}
 // As the page expands, consider pulling these out as separate React components that's exported and imported.
 // function Price() {
 //   //
